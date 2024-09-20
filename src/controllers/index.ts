c9e416f78a82788b9b07bbbb1c82a44f2c4ecc83
Yusuke4869 <67366917+Yusuke4869@users.noreply.github.com>
2024-09-21 00:08:11 +0900

    feat: implement controllers

---
+++ src/controllers/index.ts
@@ -0,0 +1,19 @@
+import type { Context } from "hono";
+
+import { getRequest } from "../services/request.ts";
+import { countUpItem, getURL } from "../services/items.ts";
+
+export const home = (c: Context) => {
+  return c.text("Hello World!");
+};
+
+export const redirect = async (c: Context) => {
+  const { host, param } = getRequest(c);
+  if (!host || !param) return c.notFound();
+
+  const url = await getURL(host, param);
+  if (!url) return c.notFound();
+
+  if (c.req.method === "GET") await countUpItem(host, param);
+  return c.redirect(url);
+};
