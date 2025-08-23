596b761946541b6901e1ceaf7555a69cca2dabb9
Yusuke <67366917+Yusuke4869@users.noreply.github.com>
2025-08-23 11:13:06 +0900

    refactor(middleware): extract middleware into dedicated directory (#19)

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
 
-import app from "./route/index.ts";
-import api from "./route/api.ts";
+import { indexRoute } from "./route/index.ts";
+import { apiRoute } from "./route/api.ts";
 
-const server = new Hono()
+const app = new Hono()
   .use(logger())
-  .route("/", app)
-  .route("/api", api);
+  .route("/", indexRoute)
+  .route("/api", apiRoute);
 
-Deno.serve(server.fetch);
+Deno.serve(app.fetch);
